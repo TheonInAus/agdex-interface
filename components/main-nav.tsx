c025c22d934b68c89491779277fb27b96f619ce3
yuyin-zhang <106600353+yuyin-zhang@users.noreply.github.com>
2023-11-28 17:17:54 +1100

    feat: 0xx name

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -20,7 +20,6 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name:'',
+  name:'0XX',
   description:
     "0XX",
   mainNav: [
