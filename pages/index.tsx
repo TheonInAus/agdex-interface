3b90832eee548c26da271933782c2f1ab5a47369
yuyin-zhang <106600353+yuyin-zhang@users.noreply.github.com>
2023-12-23 01:47:31 +1100

    fix: landingpage size

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,25 +19,37 @@ export default function IndexPage() {
 
   return (
     <section className="grid items-center">
-      <div className=" h-[500px] rounded-b-3xl" style={{ backgroundImage: 'linear-gradient(to bottom, #ADEFE7 5%, #32B4A7 95%)' }}>
-        <div className="mt-44 text-center items-center justify-center text-5xl text-black">
-          Next-Gen Decentralized Trading
-        </div>
-        <div className="text-center items-center justify-center text-base text-black mt-2">
-          Up to 200x leverage and 0 trading fees No sign up or deposit required
-        </div>
-        <div className="flex justify-center">
-          <Button
-            className="mt-5 justify-center text-base text-bronze font-semibold bg-black hover:bg-slate-900 rounded-lg"
-            onClick={handleTradeNowClick}
-          >
-            Trade Now
-            <ArrowRight className="ml-2" />
-          </Button>
+      <div
+        className=" h-[750px] rounded-b-[70px]"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, #ADEFE7 5%, #32B4A7 95%)",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center center',
+        }}
+      >
+        <div className="mt-64">
+          <div className="text-center items-center justify-center text-7xl text-black font-extrabold">
+            Next-Gen Decentralized Trading
+          </div>
+          <div className="text-center items-center justify-center text-base text-black mt-2 font-semibold">
+            Up to 200x leverage and 0 trading fees No sign up or deposit
+            required
+          </div>
+          <div className="flex justify-center">
+            <Button
+              className="mt-5 justify-center text-base text-bronze font-semibold bg-black hover:bg-slate-900 rounded-lg"
+              onClick={handleTradeNowClick}
+            >
+              Trade Now
+              <ArrowRight className="ml-2" />
+            </Button>
+          </div>
         </div>
       </div>
-      <div className="flex flex-row justify-center gap-10 mt-28 w-full">
-        <div className="w-[30%] mt-32">
+      <div className="flex flex-row justify-center gap-10 mt-28">
+        <div className="w-[400px] mt-40">
           <div className="text-white text-base">Small title</div>
           <div className="text-white text-4xl mt-1">Main title</div>
           <div className="text-0xgrey text-base mt-2">
@@ -52,7 +64,7 @@ export default function IndexPage() {
             Launch App
           </Button>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-[10px]">
           <div className="bg-0xbox rounded-lg w-[700px] h-[100px] flex gap-5 ">
             <div className="mt-10 ml-10">Icon</div>
             <div>
@@ -83,12 +95,12 @@ export default function IndexPage() {
           </div>
         </div>
       </div>
-      <div className="mt-28 flex justify-center">
-        <div className="w-[1160px] h-[170px] bg-0xbox flex gap-5 rounded-lg">
-          <div className="mt-8 ml-14">
-            <Image src="/only_logo.svg" alt="Logo" width={100} height={36} />
+      <div className="mt-40 flex justify-center items-center">
+        <div className="h-[170px] w-[1137px] bg-0xbox flex gap-5 rounded-lg">
+          <div className="mt-12 ml-14">
+            <Image src="/only_logo.svg" alt="Logo" width={70} height={36} />
           </div>
-          <div className="mx-10 mt-10">
+          <div className="mx-10 mt-12">
             <div className="text-lg text-white">Fair Launch</div>
             <div className="text-base text-0xgrey">
               EQU is the native token of Equation, with a maximum supply of 10
@@ -98,7 +110,7 @@ export default function IndexPage() {
           </div>
         </div>
       </div>
-      <div className="mt-28 flex justify-center">
+      <div className="mt-40 flex justify-center">
         <div className="text-5xl text-bronze font-semibold">The Roadmap</div>
         <div className="h-[500px]"></div>
       </div>
